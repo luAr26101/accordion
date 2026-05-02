efb1eb422b6331d747d49fc97af8512e035c6070
luAr26101 <raul.cosmin.26@icloud.com>
2026-05-02 14:49:08 +0300

    Complete app version 2.1

--- src/question.jsx
+++ src/question.jsx
@@ -1,15 +1,19 @@
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 function Question({ title, info, activeIndex, setActiveIndex, id }) {
+  const handleClick = () => {
+    if (activeIndex === id) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(id);
+    }
+  };
+
   return (
     <article className='question'>
       <header>
         <h5>{title}</h5>
-        <button
-          type='button'
-          className='question-btn'
-          onClick={() => setActiveIndex(id)}
-        >
+        <button type='button' className='question-btn' onClick={handleClick}>
           {activeIndex === id ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
